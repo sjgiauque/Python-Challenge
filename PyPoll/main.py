25b9018731aaf70f97056f1fc580d4f5f46933ca
sjgiauque <56764355+sjgiauque@users.noreply.github.com>
2019-11-10 23:07:30 -0500

    Update main.py

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,48 +1,48 @@
 # Import os module
-import os, csv
-from pathlib import Path 
+import os
+import csv
 
 # Map file location
-csv_file_path = os.path.join("python-challenge", "PyPoll", "election_data.csv")
+csvpath = os.path.join("Python-Challenge", "PyPoll", "election_data.csv")
 
-# Define Variables 
+# Define total_votes and candidate_votes as variables 
 total_votes = 0 
 khan_votes = 0
 correy_votes = 0
 li_votes = 0
 otooley_votes = 0
 
-# Open, improved reading using CSV module
-with open(csv_file_path,newline="", encoding="utf-8") as elections:
+# Open csv, improved reading using CSV module
+with open(csvpath,newline="") as elections:
 
-    # CSV reader specifies delimiter and variable that holds contents
+# CSV reader specifies delimiter and variable that holds contents
     csvreader = csv.reader(elections,delimiter=",") 
 
-    # Read the header row first (skip this step if there is now header)
-    header = next(csvreader)     
+# Skip header
+header = next(csvreader)     
 
-    # Loop through each row in the csv
-    for row in csvreader: 
+# Loop through each row in the csv
+for row in csvreader: 
 
-        # Count unique Voter ID's, tally as total_votes
-        total_votes +=1
+# Count unique Voter ID's, tally as total_votes
+    total_votes +=1
 
-        # Define/Assign candidate names, tally assigned number of votes by candidate
-        # Numerator in % vote calculation
-        if row[2] == "Khan": 
-            khan_votes +=1
-        elif row[2] == "Correy":
-            correy_votes +=1
-        elif row[2] == "Li": 
-            li_votes +=1
-        elif row[2] == "O'Tooley":
-            otooley_votes +=1
+# Define/Assign candidate names, tally assigned number of votes by candidate
+# Designate numerator in percentage vote calculation
+if row[2] == "Khan": 
+    khan_votes +=1
+elif row[2] == "Correy":
+    correy_votes +=1
+elif row[2] == "Li": 
+    li_votes +=1
+elif row[2] == "O'Tooley":
+    otooley_votes +=1
 
- # Define candidates variable using a dictionary, define votes assigned to candiates 
+ # Define candidate variable using a dictionary, define votes assigned to candiates 
 candidates = ["Khan", "Correy", "Li","O'Tooley"]
 votes = [khan_votes, correy_votes,li_votes,otooley_votes]
 
-# Zip candidates and votes variables together the list of candidate(key) and the total votes(value)
+# Zip candidate and votes variables together to correlate the list of candidates(key) and the total votes(value)
 # Return the winner using a max function of the dictionary 
 dict_candidates_and_votes = dict(zip(candidates,votes))
 key = max(dict_candidates_and_votes, key=dict_candidates_and_votes.get)
@@ -94,6 +94,3 @@ with open(output_file,"w") as file:
     file.write(f"Winner: {key}")
     file.write("\n")
     file.write(f"----------------------------")
-
-    
-    
