02d07788ee00b0610e36c914c83d86e9c3604bc9
sjgiauque <56764355+sjgiauque@users.noreply.github.com>
2019-11-10 22:20:09 -0500

    Update main.py

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,34 +1,34 @@
-# Import dependencies
+# Import os module
 import os, csv
 from pathlib import Path 
 
-# Assign file location with the pathlib library
-csv_file_path = Path("python-challenge", "PyPoll", "election_data.csv")
+# Map file location
+csv_file_path = os.path.join("python-challenge", "PyPoll", "election_data.csv")
 
-# Declare Variables 
+# Define Variables 
 total_votes = 0 
 khan_votes = 0
 correy_votes = 0
 li_votes = 0
 otooley_votes = 0
 
-# Open csv in default read mode with context manager
+# Open, improved reading using CSV module
 with open(csv_file_path,newline="", encoding="utf-8") as elections:
 
-    # Store data under the csvreader variable
+    # CSV reader specifies delimiter and variable that holds contents
     csvreader = csv.reader(elections,delimiter=",") 
 
-    # Skip the header so we iterate through the actual values
+    # Read the header row first (skip this step if there is now header)
     header = next(csvreader)     
 
-    # Iterate through each row in the csv
+    # Loop through each row in the csv
     for row in csvreader: 
 
-        # Count the unique Voter ID's and store in variable  called total_votes
+        # Count unique Voter ID's, tally as total_votes
         total_votes +=1
 
-        # We have four candidates if the name is found, count the times it appears and store in a list
-        # We can use this values in our percent vote calculation in the print statements
+        # Define/Assign candidate names, tally assigned number of votes by candidate
+        # Numerator in % vote calculation
         if row[2] == "Khan": 
             khan_votes +=1
         elif row[2] == "Correy":
@@ -38,22 +38,22 @@ with open(csv_file_path,newline="", encoding="utf-8") as elections:
         elif row[2] == "O'Tooley":
             otooley_votes +=1
 
- # To find the winner we want to make a dictionary out of the two lists we previously created 
+ # Define candidates variable using a dictionary, define votes assigned to candiates 
 candidates = ["Khan", "Correy", "Li","O'Tooley"]
 votes = [khan_votes, correy_votes,li_votes,otooley_votes]
 
-# We zip them together the list of candidate(key) and the total votes(value)
+# Zip candidates and votes variables together the list of candidate(key) and the total votes(value)
 # Return the winner using a max function of the dictionary 
 dict_candidates_and_votes = dict(zip(candidates,votes))
 key = max(dict_candidates_and_votes, key=dict_candidates_and_votes.get)
 
-# Print a the summary of the analysis
+# Calculate vote percentage of total for each candidate
 khan_percent = (khan_votes/total_votes) *100
 correy_percent = (correy_votes/total_votes) * 100
 li_percent = (li_votes/total_votes)* 100
 otooley_percent = (otooley_votes/total_votes) * 100
 
-# Print the summary table
+# Print Summary
 print(f"Election Results")
 print(f"----------------------------")
 print(f"Total Votes: {total_votes}")
@@ -66,13 +66,13 @@ print(f"----------------------------")
 print(f"Winner: {key}")
 print(f"----------------------------")
 
-# Output files
-# Assign output file location and with the pathlib library
+# Create Output files
+# Assign output file location
 output_file = Path("python-challenge", "PyPoll", "Election_Results_Summary.txt")
 
 with open(output_file,"w") as file:
 
-# Write methods to print to Elections_Results_Summary 
+# Print to Elections_Results_Summary 
     file.write(f"Election Results")
     file.write("\n")
     file.write(f"----------------------------")
@@ -94,7 +94,6 @@ with open(output_file,"w") as file:
     file.write(f"Winner: {key}")
     file.write("\n")
     file.write(f"----------------------------")
-
 
     
     
