2c9077c1d84aa0dbf500cf42b5e7981909d049cf
sjgiauque <56764355+sjgiauque@users.noreply.github.com>
2019-11-10 23:00:00 -0500

    Update main.py

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,58 +1,56 @@
-	# Import Dependencies	
-	import os, csv
-	from pathlib import Path 
+	# Import os module	
+	import os
+	import csv
+	
+	
+	# Map file location
+	input_file = os.path.join("Python-Challenge", "PyBank", "budget_data.csv")
 	
 
-	# Declare file location through pathlib
-	input_file = Path("python-challenge", "PyBank", "budget_data.csv")
-	
-
-	# Create empty lists to iterate through specific rows for the following variables
+	# Define loop variables
 	total_months = []
 	total_profit = []
 	monthly_profit_change = []
 	 
-	# Open csv in default read mode with context manager
-	with open(input_file,newline="", encoding="utf-8") as budget:
+	# Open, improved reading using CSV module
+	with open(input_file,newline="") as budget:
 	
 
-	     # Store the contents of budget_data.csv in the variable csvreader
+	     # CSV reader specifies delimiter and variable that holds contents
 	    csvreader = csv.reader(budget,delimiter=",") 
 	
 
-	    # Skip the header labels to iterate with the values
+	    # Skip header
 	    header = next(csvreader)  
 	
 
-	    # Iterate through the rows in the stored file contents
+	    # Loop through each row in the csv
 	    for row in csvreader: 
 	
 
-	        # Append the total months and total profit to their corresponding lists
+	        # Append total months and total profit to their corresponding lists
 	        total_months.append(row[0])
 	        total_profit.append(int(row[1]))
 	
 
-	    # Iterate through the profits in order to get the monthly change in profits
+	    # Loop through profits to calculate monthly change in profits
 	    for i in range(len(total_profit)-1):
 	        
-	        # Take the difference between two months and append to monthly profit change
+	        # Calculate the difference between two months and append to monthly profit change
 	        monthly_profit_change.append(total_profit[i+1]-total_profit[i])
 	        
-	# Obtain the max and min of the the montly profit change list
+	# Calculate max and min of the the montly profit change list
 	max_increase_value = max(monthly_profit_change)
 	max_decrease_value = min(monthly_profit_change)
 	
 
 	# Correlate max and min to the proper month using month list and index from max and min
-	#We use the plus 1 at the end since month associated with change is the + 1 month or next month
+	# Use the plus 1 at the end since month associated with change is the + 1 month or next month
 	max_increase_month = monthly_profit_change.index(max(monthly_profit_change)) + 1
 	max_decrease_month = monthly_profit_change.index(min(monthly_profit_change)) + 1 
 	
 
 	# Print Statements
-	
-
 	print("Financial Analysis")
 	print("----------------------------")
 	print(f"Total Months: {len(total_months)}")
